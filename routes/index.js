ca0f055e8c340751bfa4f252819d6806e2f1a333
Purrbiscuit <spurr@articulate.com>
2021-03-11 19:11:59 -0500

    remove unused variable in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const { something } = require('../public/javascripts/shared.js')
-
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' })

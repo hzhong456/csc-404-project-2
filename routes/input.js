42258aab5c983cfd6d9d5eb44650562a4fb9d66d
Purrbiscuit <spurr@articulate.com>
2021-03-20 17:08:23 -0400

    add template for views/input.ejs

--- routes/input.js
+++ routes/input.js
@@ -1,9 +1,48 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET home page. */
+const courses = {
+  csc141: 'CSC-141',
+  csc142: 'CSC-142',
+  csc240: 'CSC-240',
+  csc241: 'CSC-241',
+}
+
+const validGrades = [
+  'A',
+  'A-',
+  'B+',
+  'B',
+  'B-',
+  'C+',
+  'C',
+  'C-',
+  'D+',
+  'D',
+  'D-',
+  'F'
+]
+
+/* GET input page. */
 router.get('/', (req, res) => {
-  res.render('input')
+  res.render('input', {
+    courses,
+    validGrades
+  })
+})
+
+/* POST input page. */
+router.post('/', (req, res) => {
+  const {
+    firstName,
+    lastName,
+    csc141,
+    csc142,
+    csc240,
+    csc241
+  } = req.body
+
+  res.json(req.body)
 })
 
 module.exports = router
--- routes/qualified.js
+++ routes/qualified.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET home page. */
+/* GET qualified page. */
 router.get('/', (req, res) => {
   res.render('qualified')
 })

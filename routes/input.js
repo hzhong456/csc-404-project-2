7e150560a63bcdec494a652ee19b7b2bb9947b00
Purrbiscuit <spurr@articulate.com>
2021-03-20 18:13:08 -0400

    add calculate gpa functionality to the input route

--- routes/input.js
+++ routes/input.js
@@ -3,8 +3,31 @@ const router = express.Router()
 
 const {
   courses,
-  gradeScale
+  gradeScale,
+  students
 } = require('../lib/data')
+
+const {
+  calcGPA
+} = require('../lib/calculate')
+
+const formatStudentRecord = ({
+  firstName,
+  lastName,
+  csc141,
+  csc142,
+  csc240,
+  csc241
+}) => ({
+  firstName,
+  lastName,
+  courseGrades: {
+    csc141,
+    csc142,
+    csc240,
+    csc241
+  }
+})
 
 /* GET input page. */
 router.get('/', (req, res) => {
@@ -16,16 +39,18 @@ router.get('/', (req, res) => {
 
 /* POST input page. */
 router.post('/', (req, res) => {
-  // const {
-  //   firstName,
-  //   lastName,
-  //   csc141,
-  //   csc142,
-  //   csc240,
-  //   csc241
-  // } = req.body
+  const student = formatStudentRecord(req.body)
 
-  res.json(req.body)
+  students.push(student)
+
+  const partialGPA = calcGPA(student)
+
+  res.render('input', {
+    courses,
+    gradeScale,
+    student,
+    partialGPA
+  })
 })
 
 module.exports = router
--- routes/qualified.js
+++ routes/qualified.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const {
+  students
+} = require('../lib/data')
+
 /* GET qualified page. */
 router.get('/', (req, res) => {
+  console.log(students)
   res.render('qualified')
 })
 

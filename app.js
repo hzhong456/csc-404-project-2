bf398bec1601374edc9ee8285b9dd4d3fc709584
Purrbiscuit <spurr@articulate.com>
2021-03-20 13:56:41 -0400

    setup the bones of the project

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const inputRouter = require('./routes/input')
+const qualifiedRouter = require('./routes/qualified')
 
 const app = express()
 
@@ -20,7 +21,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/input', inputRouter)
+app.use('/qualified', qualifiedRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

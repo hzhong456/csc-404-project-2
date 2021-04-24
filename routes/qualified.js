099b7569251238180c387b97b1fa2f0d35c77b83
hzhong456 <68069530+hzhong456@users.noreply.github.com>
2021-04-23 21:09:44 -0400

    updated qualified view

--- routes/qualified.js
+++ routes/qualified.js
@@ -4,17 +4,37 @@ const router = express.Router()
 const { calcGPAs } = require('../lib/calculate')
 const { students } = require('../lib/data')
 const { filterQualified, sortStudents } = require('../lib/student')
+const studentRecords = require('../lib/model/student_model')
 
 /* GET qualified page. */
-router.get('/', (req, res) => {
-  const studentsWithGPAs = calcGPAs(students)
-  const qualifiedStudents = filterQualified(studentsWithGPAs)
-  const sortedStudents = sortStudents(qualifiedStudents)
+router.get('/', (req, res, next) => {
 
-  res.render('qualified', {
-    students: sortedStudents,
-    totalStudents: students.length
-  })
+  studentRecords.find()
+    .then(records => {
+      const studentsWithGPAs = calcGPAs(records)
+      const qualifiedStudents = filterQualified(studentsWithGPAs)
+      const sortedStudents = sortStudents(qualifiedStudents)
+      res.render('qualified', {
+        students: sortedStudents,
+        totalStudents: records.length
+      })
+      next();
+    })
+    .catch(error => {
+      console.log(`Error fetching users: ${error.message}`);
+      next(error);
+
+      // const studentsWithGPAs = calcGPAs(students)
+
+      // const qualifiedStudents = filterQualified(studentsWithGPAs)
+
+      // const sortedStudents = sortStudents(qualifiedStudents)
+
+      // res.render('qualified', {
+      //   students: sortedStudents,
+      //   totalStudents: students.length
+      // })
+    })
 })
 
 module.exports = router
